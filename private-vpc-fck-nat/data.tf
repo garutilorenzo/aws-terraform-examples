88f528e918e0f947c2f4da3b8ee2c819e824d6eb
Lorenzo Garuti <garuti.lorenzo@gmail.com>
2025-08-18 09:48:20 +0000

    Added VPC without NAT GW. This module needs to be used with fck-nat module

--- private-vpc-fck-nat/data.tf
+++ private-vpc-fck-nat/data.tf
@@ -1,3 +1,8 @@
 data "aws_availability_zones" "available_az" {
   state = "available"
 }
+
+data "aws_availability_zone" "az" {
+  for_each = toset(data.aws_availability_zones.available_az.zone_ids)
+  zone_id  = each.key
+}
